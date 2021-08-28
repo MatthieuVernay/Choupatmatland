ed7ee5594a660cab13d31dcf3a17f1568749aba8
Patamout <matth.epsi@gmail.com>
2021-08-28 14:15:35 +0200

    Ajout du switch
    Switch pour les différentes actions

--- Choupatmatland/Program.cs
+++ Choupatmatland/Program.cs
@@ -12,58 +12,101 @@ namespace Choupatmatland
         {
             Console.WriteLine("Jeu du Orochimaru la crevette !\n");
 
-            //Affrontement entre Patamout "ammenez moi vers la bouffe" et Choupat "C'est mon royauuuuuume, INSECTE !"  VS Orochimaru et kabuto les crevettes des profondeurs
+            //Affrontement entre Patamout et Choupat VS Orochimaru et kabuto les crevettes des profondeurs
 
+            //Actions : Attaque, Attaque spéciale, Soin, Invocation, Modes
+
+            //A FAIRE
             //Patamout combat toujours Orochimaru la crevette
             //Choupat combat toujours Kabuto la crevette 
             //Ensuite ils nous attaquent
-            //les points de vie de patamout et de choupat sont a 5000 et les points de vie de Orochimaru et kabuto sont a 4000
+            //Tous le monde peut esquiver les attaques : Patamout et Choupat 40% d'esquive, Orochimaru et Kabuto 30% 
+            //Tous le monde peut se soigner sois même (700 pv) ou son allié (500 pv) 40% de chance de se soigner, cible aléatoire entre sois ou allié, on peut obtenir plus de pv que les pv initiaux
+            //tous peuvent rater une attaque/spéciale/mode 20%
+            //Créer pour chaque personnages un cri de guerre : Patamout -> "ammenez moi vers la bouffe" , Choupat -> "C'est mon royauuuuuume, INSECTE !"
+            //1 action à la fois
+
+            //FAIT 
+            //les points de vie de patamout et de choupat sont a 6000 et les points de vie de Orochimaru et kabuto sont a 5700
             //les points d'attaque de patamout et de choupat sont a 700 et les points d'attaque de Orochimaru et kabuto sont a 450
             //les attaque spéciales de patamout et de choupat sont "les loupi sont de sortie 750", crachat de boule de poil 780 et battement de queue de fer 950
             //les attaque de Orochimaru et kabuto sont la morsure du serpent 700, les lames de chakra 800 et coup de boule 1000
             //les modes de patamout et choupat sont le chakra de loopy 300 , la queue du demon loopy 350 et fusion demon loopy patamout\choupat 500
             //les modes de Orochimaru et kabuto sont le chakra de la crevette 350, la queue de la crevette 400 et la fusion crevette Orochimaru/kabuto 600
-            //Tous le monde peut esquiver les attaques : Pamatou et Choupat 40% d'esquive, Orochimaru et Kabuto 30% 
             //On peut faire une invocation loopy géant 1580, crevette géante 1550
-            //Tous le monde peut se soigner sois même (700 pv) ou son allié (500 pv) 40% de chance de se soigner, cible aléatoire entre sois ou allié, on peut obtenir plus de pv que les pv initiaux
-            //tous peuvent rater une attaque/spéciale/mode 20%
 
+            //Nom des personnages
             string choupat = "Choupat";
             string patamout = "Patamout";
             string orochimaru = "Orochimaru";
             string kabuto = "Kabuto";
 
+            //Cri de guerre
+            string criPatamout = "ammenez moi vers la bouffe";
+            string criChoupat = "C'est mon royauuuuuume, INSECTE !";
+            string criOrochimaru = "";
+            string criKabuto = "";
+
+            //Pv des personnages
             int PvPatamout = 5000;
             int PvChoupat = 5000;
             int PvOrochimaru = 4000;
             int PvKabuto = 4000;
 
+            //Attaque des personnages
             int Atkpatamout = 700;
             int AtkChoupat = 700;
             int AtkOrochima = 450;
             int AtkKabuto = 450;
 
+            //Nos attaques spéciales
             int atkLoopyDeSortie = 750;
             int crachatBoulePoil = 750;
             int queueDeFer = 750;
+
+            //Attaque spéciales des ennemies
             int morsureSerpent = 700;
             int lameChakra = 800;
             int coupdeboule = 1000;
 
+            //Nos modes
             int modChakraLoopy = 300;
             int queueDémonLoopy = 350;
             int fusionDémonLoopy = 500;
-<<<<<<< HEAD
+
+            //Mode des ennemies
             int chakraCrevette = 350;
             int queuedémoncrevette = 400;
             int fusionCrevette = 600;
 
+            //Invocations
             int loopyGéant = 1580;
             int miniLoopy = 1800;
             int crevetteGéante = 1550;
-            int minicrevette = 1800; 
-=======
->>>>>>> main
+            int minicrevette = 1800;
+
+            Random rand = new Random();
+            int choix = rand.Next(1, 6);
+
+
+            int Actions(/*string personnages*/)
+            {
+                switch (choix)
+                {
+                    case 1:
+                        //attaque 
+                        break;
+
+                }
+
+                return 
+            }
+
+
+
+
+
+
 
 
             /*List<string> listeDeMonstre = new List<string>();
