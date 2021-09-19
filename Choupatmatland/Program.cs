b5d1a732e10b08574871ce39df8e39c8686b2049
Patamout <matth.epsi@gmail.com>
2021-09-19 10:15:31 +0200

    Ajout fonctionnalités
    Pour le soin
    Pour les invocations

--- Choupatmatland/Program.cs
+++ Choupatmatland/Program.cs
@@ -13,29 +13,7 @@ namespace Choupatmatland
             Console.WriteLine("Jeu du Orochimaru la crevette !\n");
 
             //Affrontement entre Patamout et Choupat VS Orochimaru et kabuto les crevettes des profondeurs
-
             //Actions : Attaque, Attaque spéciale, Soin, Invocation, Modes
-
-            //A FAIRE
-            //Patamout combat toujours Orochimaru la crevette
-            //Choupat combat toujours Kabuto la crevette 
-            //Ensuite ils nous attaquent
-            //Tous le monde peut esquiver les attaques : Patamout et Choupat 40% d'esquive, Orochimaru et Kabuto 30% 
-            //Tous le monde peut se soigner sois même (700 pv) ou son allié (500 pv) 40% de chance de se soigner, cible aléatoire entre sois ou allié, on peut obtenir plus de pv que les pv initiaux
-            //tous peuvent rater une attaque/spéciale/mode 20%
-            //Créer pour chaque personnages un cri de guerre : Patamout -> "ammenez moi vers la bouffe" , Choupat -> "C'est mon royauuuuuume, INSECTE !"
-            //1 action à la fois
-            //ressucité un allié 1 fois par partie -> 5% de chançe
-
-            //FAIT 
-            //les points de vie de patamout et de choupat sont a 6000 et les points de vie de Orochimaru et kabuto sont a 5700
-            //les points d'attaque de patamout et de choupat sont a 700 et les points d'attaque de Orochimaru et kabuto sont a 450
-            //les attaque spéciales de patamout et de choupat sont "les loupi sont de sortie 750", crachat de boule de poil 780 et battement de queue de fer 950
-            //les attaque de Orochimaru et kabuto sont la morsure du serpent 700, les lames de chakra 800 et coup de boule 1000
-            //les modes de patamout et choupat sont le chakra de loopy 300 , la queue du demon loopy 350 et fusion demon loopy patamout\choupat 500
-            //les modes de Orochimaru et kabuto sont le chakra de la crevette 350, la queue de la crevette 400 et la fusion crevette Orochimaru/kabuto 600
-            //On peut faire une invocation loopy géant 1580, crevette géante 1550
-
 
             Random rand = new Random();
             int choix = rand.Next(1, 6);
@@ -70,12 +48,34 @@ namespace Choupatmatland
             Choupat.PointATK = 700;
             Choupat.NomPerso = "Choupat";
             Choupat.CriDeGuerre = "C'est mon royauuuuuume, INSECTE !";
+            Console.WriteLine("Les pv du choupat sont à : {0}", Choupat.PointDeVie);
 
             Personnage Patamout = new Personnage();
             Patamout.PointDeVie = 5500;
             Patamout.PointATK = 750;
             Patamout.NomPerso = "Patamout";
             Patamout.CriDeGuerre = "Ammenez moi vers la bouffe !";
+
+            int soinAllie = 500;
+            int soinsoimeme = 700;
+
+            Random rand2 = new Random();
+            int choixSoin = rand.Next(1, 101);
+            Console.WriteLine("Choix Soin = " + choixSoin);
+            if(choixSoin <= 40)
+            {
+                Random rand3 = new Random();
+                int choixSoinPersonne = rand.Next(1, 3);
+                Console.WriteLine("Choix Soin Personne = " + choixSoinPersonne);
+                if(choixSoinPersonne == 1)
+                {
+                    Choupat.PointDeVie += soinsoimeme;
+                }
+                else
+                {
+                    Patamout.PointDeVie += soinAllie;
+                }
+            }
             
             Personnage Orochimaru = new Personnage();
             Orochimaru.PointDeVie = 4500;
@@ -200,14 +200,18 @@ namespace Choupatmatland
     {
         loopyGéant = 1580,
         miniLoopy = 1800,
-        loopyFou = 2000
+        loopyFou = 2000,
+        patamoutfou = 3999,
+        choupatfou = 3500
     }
 
     enum LesinvocationEnemies
     {
         crevetteGéante = 1550,
         minicrevette = 1800,
-        crevetteFolle = 2000
+        crevetteFolle = 2000,
+        orochimarucrevette = 3999,
+        kabutocrevette = 3500
     }
 
 }
